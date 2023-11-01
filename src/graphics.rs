f0d1baa740b1a060f61732c58ac2e4e5a6db37d8
dustin4242 <thegameexposer@gmail.com>
2023-11-01 11:08:55 -0500

    converted the graphics printing over to the new format

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,7 +1,4 @@
-use crate::{
-    print,
-    screen::{SCREEN_HEIGHT, SCREEN_WIDTH},
-};
+use crate::screen::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
 pub fn cat_graphic() -> &'static [u8] {
     include_bytes!("./cat.gf")
@@ -11,7 +8,7 @@ pub struct Graphics {
     pub width: u16,
     pub height: u16,
     pub data: [[u8; 16]; SCREEN_WIDTH / 8 * SCREEN_HEIGHT / 16],
-    pub color_pallete: Option<&'static [u8]>,
+    pub color_pallete: Option<[(u8, u8, u8); 8]>,
     pub color_data: Option<&'static [u8]>,
 }
 
@@ -19,10 +16,6 @@ pub fn load_gf(graphic: &'static [u8]) -> Graphics {
     let width: u16 = graphic[0] as u16;
     let height: u16 = graphic[1] as u16;
     let graphical_data = graphic.get(2..(width * height * 16) as usize + 2).unwrap();
-    let mut color_index = (width * height * 16) as usize + 2;
-    let color_pallete = graphic.get(color_index..color_index + 4);
-    color_index += 4;
-    let color_data = graphic.get(color_index..);
     let mut data: [[u8; 16]; SCREEN_WIDTH / 8 * SCREEN_HEIGHT / 16] =
         [[0; 16]; SCREEN_WIDTH / 8 * SCREEN_HEIGHT / 16];
     for y in 0..height {
@@ -36,6 +29,21 @@ pub fn load_gf(graphic: &'static [u8]) -> Graphics {
             data[(y * width + x) as usize] = character;
         }
     }
+    let mut color_index = (width * height * 16) as usize + 2;
+    let mut color_pallete = None;
+    if graphic.get(color_index..color_index + 8 * 3).is_some() {
+        let mut temp: [(u8, u8, u8); 8] = [(0, 0, 0); 8];
+        for x in 0..8 {
+            let mut rgb = (0, 0, 0);
+            rgb.0 = *graphic.get(color_index + x * 3).unwrap();
+            rgb.1 = *graphic.get(color_index + x * 3 + 1).unwrap();
+            rgb.2 = *graphic.get(color_index + x * 3 + 2).unwrap();
+            temp[x] = rgb;
+        }
+        color_pallete = Some(temp);
+    }
+    color_index += 8 * 3;
+    let color_data = graphic.get(color_index..);
     Graphics {
         width,
         height,
